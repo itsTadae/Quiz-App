c47ff2659e912aa75c8c3193198bf190778f3538
Matthew Henry <tadaemusic@gmail.com>
2020-04-04 15:25:11 -0400

    Edits
    Fixed stuff that lead to more stuff breaking

--- api.js
+++ api.js
@@ -1,10 +1,10 @@
-const startBtn = document.getElementById('start-btn')
-const nextBtn = document.getElementById('next-btn')
-const body = document.getElementById('bodyElement')
-var score = 0;
-const questionContainerElement = document.getElementById('question-container')
-const questionElement = document.getElementById('question')
-const answerBtnsElement = document.getElementById('answer-buttons')
+const startBtn = document.getElementById('start-btn');
+const nextBtn = document.getElementById('next-btn');
+const body = document.getElementById('bodyElement');
+let score = 0;
+let questionContainerElement = document.getElementById('question-container');
+const questionElement = document.getElementById('question');
+const answerBtnsElement = document.getElementById('answer-buttons');
 
 
 let shuffledQuestions, currentQuestionIndex
@@ -15,8 +15,11 @@ nextBtn.addEventListener('click', () => {
     clearStatusClass(body)
     setNextQuestion()
 })
-
+// Start quiz
 function startGame() {
+    this.questionContainerElement = document.getElementById('question-container');
+    resetState()
+    renderCounter()
     startBtn.classList.add('hide')
     shuffledQuestions = questions.sort(() => Math.random() - .5)
     this.score = 0
@@ -26,13 +29,14 @@ function startGame() {
     setNextQuestion()
     
 }
-
+//Cycle questions
 function setNextQuestion() {
     resetState()
     showQuestion(shuffledQuestions[currentQuestionIndex])
-    document.getElementById("score").innerHTML = "Score: " + score;
+    //document.getElementById("score").innerHTML = "Score: " + score;
+    
 }
-
+// Display questions
 function showQuestion(question) {
     questionElement.innerText = question.question
     question.answers.forEach(answer => {
@@ -49,14 +53,14 @@ function showQuestion(question) {
         
     })
 }
-
+// Reset question state
 function resetState() {
     nextBtn.classList.add('hide')
     while (answerBtnsElement.firstChild) {
         answerBtnsElement.removeChild(answerBtnsElement.firstChild)
     }
 }
-
+// Selecting answer
 function selectAnswer(e) {
     const selectedBtn = e.target
     const correct = selectedBtn.dataset.correct
@@ -67,35 +71,60 @@ function selectAnswer(e) {
     if (shuffledQuestions.length > currentQuestionIndex + 1){
         nextBtn.classList.remove('hide')
     } else {
-        
         startBtn.innerText = 'Restart'
         startBtn.classList.remove('hide')
+        showScores(); 
     }
 }
-
+// correct / wrong decision 
 function setStatusClass(element, correct) {
     clearStatusClass(element)
     if (correct) {
         element.classList.add('correct')
-        score++;
+        score++;     
         
         
     } else {
         element.classList.add('wrong')
         
+            
+
     }
 }
-
+// Clear status
 function clearStatusClass(element) {
     element.classList.remove('correct')
     element.classList.remove('wrong')
 }
-
+// Quiz score
 function showScores() {
+    
     var gameOverHTML = "<h1>Result</h1>";
     gameOverHTML += "<h2 id='score'> Your scores: " + score + "</h2>";
-    var element = document.getElementById("quiz");
-    element.innerHTML = gameOverHTML;
-};
+    questionContainerElement.innerHTML = gameOverHTML;
+    startBtn.innerText = 'Restart';
+    startBtn.classList.remove('hide');
 
+    
+}
+
+
+
+// TIMER
+const startingSeconds = 30;
+let time = 30;
+const countdownEl = document.getElementById('countdown');
+
+setInterval(renderCounter, 1000);
+
+function renderCounter() {
+    if (time = time) {
+        countdownEl.innerHTML = time;
+        time--;
+    } else {
+        time = 0
+        clearInterval(countdown);
+        showScores();
+    }
+}
 
--- script.js
+++ script.js
@@ -2,21 +2,59 @@
 
 const questions = [
     {
-        question: "what is 2+2?",
+        question: "What was the first champion designed? ",
         answers: [
-            {text:  "4", correct: true},
-            {text: "22", correct: false}
+            {text:  "Annie", correct: false},
+            {text: "Lux", correct: false},
+            {text: "Thresh", correct: false},
+            {text: "Singed", correct: true}
         ]
     },
     {
-        question: "Is this cool?",
+        question: "Out of the following Champions, which one does NOT share the same voice as the other Champions?",
         answers: [
-            {text: "yes", correct: true},
-            {text: "nope", correct: false},
-            {text: "no", correct: false},
-            {text: "yep", correct: true}
+            {text: "Zyra", correct: false},
+            {text: "Caitlyn", correct: true},
+            {text: "Shyvana", correct: false},
+            {text: "Elise", correct: false}
         ]
-    }    
+    },
+    {
+        question: "League of legends is the highest paying competitive eSport in the World.",
+        answers: [
+            {text:  "True", correct: false},
+            {text: "False", correct: true}
+        ]
+    },
+    {
+        question: "What Champion was inspired by a Lord of the Rings character?",
+        answers: [
+            {text: "Darius", correct: false},
+            {text: "Garen", correct: false},
+            {text: "Aatrox", correct: true},
+            {text: "Ekko", correct: false}
+        ]
+    }        
 
 
 ]
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
